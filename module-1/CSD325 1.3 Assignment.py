6044b9e022cdba982e4ca5b6b53a57083c1432d8
Weihao Fu <60823421+fwh619070942@users.noreply.github.com>
2024-11-24 18:36:38 -0500

    adding comments

--- module-1/CSD325 1.3 Assignment.py
+++ module-1/CSD325 1.3 Assignment.py
@@ -1,5 +1,12 @@
+# Weihao
+#11/24/2024
+#CSD325-T301 1.3 Assignment
+#The purpose of the code is to utilize the song "100 bottles of beer on the wall" game to practice my python code
+
+#Input the Number Of Bottles
 numbOfBottles = int(input("Enter number of bottles: "))
 
+#While Number of Bottles are greater than 0, the game continues, else game ends.
 while numbOfBottles >0:
     totalBottlesOnTheWall = numbOfBottles - 1
     print (f'{numbOfBottles} bottles of beer on the wall, {numbOfBottles} bottles of beer.')
