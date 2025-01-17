c6461bc9f7bc02e0c549c0d94617a13c37de5d84
Weihao Fu <60823421+fwh619070942@users.noreply.github.com>
2025-01-16 23:07:43 -0500

    Adding module 6 along with assignment

---
+++ module-6/forestfiresim_325.py
@@ -0,0 +1,142 @@
+"""Weihao Fu
+    01/16/2025
+    CSD325 6.2 Assignment"""
+"""Forest Fire Sim, modified by Sue Sampson, based on a program by Al Sweigart
+A simulation of wildfires spreading in a forest. Press Ctrl-C to stop.
+Inspired by Nicky Case's Emoji Sim http://ncase.me/simulating/model/
+** use spaces, not indentation to modify **
+Tags: short, bext, simulation"""
+
+import random, sys, time
+
+try:
+    import bext
+except ImportError:
+    print('This program requires the bext module, which you')
+    print('can install by following the instructions at')
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
+
+# Set up the constants:
+WIDTH = 75
+HEIGHT = 22
+
+TREE = 'A'
+FIRE = '@'
+EMPTY = ' '
+#Added river 
+RIVER = 'W'
+
+# (!) Try changing these settings to anything between 0.0 and 1.0:
+INITIAL_TREE_DENSITY = 0.5  # Amount of forest that starts with trees.
+GROW_CHANCE = 0.01  # Chance a blank space turns into a tree.
+FIRE_CHANCE = 0.01  # Chance a tree is hit by lightning & burns.
+
+# (!) Try setting the pause length to 1.0 or 0.0:
+PAUSE_LENGTH = 1.0
+
+
+def main():
+    forest = createNewForest()
+
+    bext.clear()
+
+    while True:  # Main program loop.
+        displayForest(forest)
+
+        # Run a single simulation step:
+        nextForest = {'width': forest['width'],
+                      'height': forest['height']}
+
+        for x in range(forest['width']):
+            for y in range(forest['height']):
+                if (x, y) in nextForest:
+                    # If we've already set nextForest[(x, y)] on a
+                    # previous iteration, just do nothing here:
+                    continue
+
+                if ((forest[(x, y)] == EMPTY)
+                    and (random.random() <= GROW_CHANCE)):
+                    # Grow a tree in this empty space.
+                    nextForest[(x, y)] = TREE
+                elif ((forest[(x, y)] == TREE)
+                    and (random.random() <= FIRE_CHANCE)):
+                    # Lightning sets this tree on fire.
+                    nextForest[(x, y)] = FIRE
+                elif forest[(x, y)] == FIRE:
+                    # This tree is currently burning.
+                    # Loop through all the neighboring spaces:
+                    for ix in range(-1, 2):
+                        for iy in range(-1, 2):
+                            # Fire spreads to neighboring trees:
+                            if forest.get((x + ix, y + iy)) == TREE:
+                                nextForest[(x + ix, y + iy)] = FIRE
+                    # The tree has burned down now, so erase it:
+                    nextForest[(x, y)] = EMPTY
+                else:
+                    # Just copy the existing object:
+                    nextForest[(x, y)] = forest[(x, y)]
+        forest = nextForest
+        ##createNewRiver(river)
+        time.sleep(PAUSE_LENGTH)
+
+
+def createNewForest():
+    """Returns a dictionary for a new forest data structure with a rectangular river."""
+    forest = {'width': WIDTH, 'height': HEIGHT}
+    #Define the width of the river is 5
+    river_width = 5 
+    #Define the height of the river is 10
+    river_height = 10 
+    # Define the Width starting point of the river
+    start_river_x = (WIDTH - river_width) // 2
+    #Define the Width ending point of the river
+    end_river_x = start_river_x + river_width
+    #Define the Height starting point of the river
+    start_river_y = (HEIGHT - river_height) // 2 
+    #Define the Height ending point of the river
+    end_river_y = start_river_y + river_height
+
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            #Start as a River
+            if start_river_x <= x < end_river_x and start_river_y <= y < end_river_y:
+                forest[(x, y)] = RIVER
+            elif (random.random() * 100) <= INITIAL_TREE_DENSITY:
+                forest[(x, y)] = TREE  # Start as a tree.
+            else:
+                forest[(x, y)] = EMPTY  # Start as an empty space.
+    return forest
+
+
+def displayForest(forest):
+    """Display the forest data structure on the screen."""
+    bext.goto(0, 0)
+    for y in range(forest['height']):
+        for x in range(forest['width']):
+            if forest[(x, y)] == TREE:
+                bext.fg('green')
+                print(TREE, end='')
+            elif forest[(x, y)] == FIRE:
+                bext.fg('red')
+                print(FIRE, end='')
+            #Added River as blue
+            elif forest [(x, y)] == RIVER:
+                bext.fg('blue')
+                print(RIVER, end='')
+            elif forest[(x, y)] == EMPTY:
+                print(EMPTY, end='')
+        print()
+    bext.fg('reset')  # Use the default font color.
+    print('Grow chance: {}%  '.format(GROW_CHANCE * 100), end='')
+    print('Lightning chance: {}%  '.format(FIRE_CHANCE * 100), end='')
+    print('Press Ctrl-C to quit.')
+
+
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()  # When Ctrl-C is pressed, end the program.
